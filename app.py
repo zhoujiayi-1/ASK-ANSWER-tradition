4845d6c1e7b70857a522f2df56c2ce78b9b0d302
zhoujiayi-1 <15324449707@163.com>
2026-06-04 19:55:33 +0800

    Update app.run to use environment variable for port
    Change app to use environment variable for port and disable debug mode.

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def statistics():
     return jsonify(stats)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
